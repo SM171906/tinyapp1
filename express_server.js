766c4257fa1370706d75cad53c15189c980e87fb
Smiti <mishra.smiti06@gmail.com>
2021-08-31 15:28:38 -0400

    update the file

--- express_server.js
+++ express_server.js
@@ -5,58 +5,66 @@ function generateRandomString() {
   for (let i = 0; i < charactersLength; i++) {
     result += characters.charAt(Math.floor(Math.random() *
       charactersLength));
-    
+
   }
   return result.substring(0, 6);
-  }
 }
 
-  const express = require("express");
-  const app = express();
-  const PORT = 8080; // default port 8080
 
-  const bodyParser = require("body-parser");
-  app.use(bodyParser.urlencoded({ extended: true }));
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
-  app.post("/urls", (req, res) => {
-    console.log(req.body.longURL);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  });
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.set("view engine", "ejs");
-  const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  };
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  console.log(req.params.shortURL);
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.set("view engine", "ejs");
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
-  app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
-  app.get("/urls/:shortURL", (req, res) => {
-    let lu = urlDatabase[req.params.shortURL];
-    const templateVars = { shortURL: req.params.shortURL, longURL: lu };
-    res.render("urls_show", templateVars);
-  });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+  let lu = urlDatabase[req.params.shortURL];
+  const templateVars = { shortURL: req.params.shortURL, longURL: lu };
+  res.render("urls_show", templateVars);
+});
 
-  app.get("/", (req, res) => {
-    res.send("Hello!");
-  });
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-  console.log(generateRandomString());
-  console.log(generateRandomString());
-  console.log(generateRandomString());
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
-  app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-  });
-  
+
+
+console.log(generateRandomString());
+console.log(generateRandomString());
+console.log(generateRandomString());
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
